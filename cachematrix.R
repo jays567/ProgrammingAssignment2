acbc1980d603e438182856e38d7ca8de8c8e2452
jays567 <jays567@gmail.com>
2015-04-22 16:56:41 -0700

    Assignment #2
    Updated JB

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## In this script there are two functions: 1) returns caches an inverse of a supplied matrix, and 2) computes the inverse of a square matrix.
 
 ## This function creates a Cache of the inverse of a matrix. 
+
 makeCacheMatrix <- function(x = matrix()) {
 cacheStore <- NULL
 set <- function(y) {
@@ -13,9 +14,13 @@ getinverse <- function() cacheStore
 list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-## This function creates a Cache of the inverse of a matrix. 
+
+## This function creates a Cache of the inverse of a matrix.
+
 cacheSolve <- function(x, ...) {
+  
   ## Return a matrix that is the inverse of 'x'
+  
   m <- x$getinverse()
   if(!is.null(cacheStore)) {
     message ("getting cached data")
